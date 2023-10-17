bb84014d76a35925faad1193652b628b24f3dadc
Cristóbal José Jiménez Gómez <cristobal_jjg@hotmail.com>
2023-10-17 09:34:47 +0100

    Manejo de errores construido como map

--- src/app/services/error-manager.service.ts
+++ src/app/services/error-manager.service.ts
@@ -4,34 +4,22 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ErrorManagerService {
+  private errors = new Map()
+    .set('ok', 'ok')
+    .set('sameThanBefore', 'error.sameBefore')
+    .set('auth/email-already-exists', 'error.exists')
+    .set('auth/insufficient-permission', 'error.perm')
+    .set('auth/internal-error', 'error.intern')
+    .set('auth/invalid-email', 'error.invalid-mail')
+    .set('auth/invalid-password', 'error.invalid-pass')
+    .set('auth/user-not-found', 'error.not-found')
+    .set('auth/invalid-login-credentials', 'error.invalid-cred')
+    .set('auth/too-many-requests', 'error.many-tries')
+    .set('', '');
   constructor() {}
 
   manageErrors(e: string) {
-    console.log(e);
-
-    switch (e) {
-      case 'ok':
-        return 'ok';
-      case 'sameThanBefore':
-        return 'error.sameBefore';
-      case 'auth/email-already-exists':
-        return 'error.exists';
-      case 'auth/insufficient-permission':
-        return 'error.perm';
-      case 'auth/internal-error':
-        return 'error.intern';
-      case 'auth/invalid-email':
-        return 'error.invalid-mail';
-      case 'auth/invalid-password':
-        return 'error.invalid-pass';
-      case 'auth/user-not-found':
-        return 'error.not-found';
-      case 'auth/invalid-login-credentials':
-        return 'error.invalid-cred';
-      case 'auth/too-many-requests':
-        return 'error.many-tries';
-      default:
-        return 'error.chungo';
-    }
+    if (this.errors.has(e)) return this.errors.get(e);
+    else return 'error.chungo';
   }
 }
